481cd794365ec4fe2904aa799dc198ed388607da
HARU <hahntb0501@gmail.com>
2017-11-13 14:16:49 +0000

    follow,unfollow

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,5 +11,7 @@ class ApplicationController < ActionController::Base
   end
   def counts(user)
       @count_micropost = user.microposts.count
+      @count_followings = user.followings.count
+      @count_followers = user.followers.count
   end
 end
